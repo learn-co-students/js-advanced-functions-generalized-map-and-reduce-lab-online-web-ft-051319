c2b601eefc3dfec082ac23b9a327e99588694083
github username <github email address>
2019-08-27 11:52:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+function map(src, fn) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(fn(src[i])) // Unique work
+    }
+    return r
+  }
+
+  function reduce(src, fn, starting = 0){
+    let result;
+    let i;
+    if(starting){
+        result = starting
+    }else{
+        result = src[0]
+    }
+
+    if(!!starting){
+        i = 0
+    }else{
+        i = 1
+    }
+
+    for(; i < src.length; i++){
+        result = (fn(src[i], result))
+    }
+    return result;
+  }
