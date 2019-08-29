04e8f17d2d2fb1aafac4096f0185d70dd99c5d0b
jenniferbusca <jennifer.busca@gmail.com>
2019-08-28 22:00:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(array, block){
+  return array.map(x => block(x))
+}
+
+function reduce(array, block, startingValue = 0){
+  return array.reduce((current, next) => block(current, next, startingValue = 0))
+}
+//
+// function reduce(array, block, startingValue){
+//   return array.reduce(function (current, next, startingValue) {
+//     if(startingValue === undefined){
+//       return block(current, next)
+//     }
+//     else {
+//       return block(current, next, startingValue)
+//     }
+//
+//   })
+// }
