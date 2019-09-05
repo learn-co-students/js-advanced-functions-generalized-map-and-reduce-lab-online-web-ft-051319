a8c5c17ed0acf94a4fbdbf35e4bf5c2addf4fbed
Natalie <natsetc@gmail.com>
2019-09-05 16:17:51 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+function map(array, fxn) {
+    let r = [];
+    for (let i = 0; i < array.length; i++ ) {
+        r.push(fxn(array[i]));
+    }
+    return r;
+}
+
+function reduce(array, fxn, startingPoint = 0) {
+    let memo = (!!startingPoint) ? startingPoint : array[0]
+    for (let i = (!!startingPoint) ? 0 : 1 ; i < array.length; i++ ) {
+        memo = fxn(array[i], memo);
+    }
+    return memo;
+}
